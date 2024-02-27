b481274e7c0c3d95b5bc35d27f709ec2b27fa3e9
Mikael Tiba <mikaeltiba04@gmail.com>
2024-02-27 13:30:56 -0400

    Update files

--- modulo02/funcion02/app.py
+++ modulo02/funcion02/app.py
@@ -1,6 +1,18 @@
-def suma(a,b):
+def suma(a=0,b=0):
     return (a + b)
 
 resultado = suma(10, 5)
+
+print(resultado)
+
+resultado = suma(12, 4)
+
+print(resultado)
+
+resultado = suma(1)
+
+print(resultado)
+
+resultado = suma()
 
 print(resultado)
