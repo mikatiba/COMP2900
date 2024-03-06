cc26b048595ce26a552944d91b5d23453a8a3d59
Mikael Tiba <mikaeltiba04@gmail.com>
2024-03-06 16:37:26 -0400

    Update ejercicio 5 y6

---
+++ modulo02/AC-funciones-arreglos/ej06.py
@@ -0,0 +1,11 @@
+def invertir_cadena(cadena):
+    return ''.join(reversed(cadena))#reversed permite inverrtir los caracteres
+
+texto = "Hola mi nombre es Mikael"
+
+cadena_invertida = invertir_cadena(texto)
+
+print(cadena_invertida)
+
+
+
