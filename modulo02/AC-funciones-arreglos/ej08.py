1fa9eeb1aa2b7a4b9260a0e9c606224befec3ef9
Mikael Tiba <mikaeltiba04@gmail.com>
2024-03-07 21:58:36 -0400

    Update ejercicios

--- modulo02/AC-funciones-arreglos/ej08.py
+++ modulo02/AC-funciones-arreglos/ej08.py
@@ -2,7 +2,7 @@ def contar_vocales(cadena):
     contador_vocales = 0 #se inicializa en 0
 
     for letra in cadena:
-        if letra.lower() in "aeiou": #
+        if letra.lower() in "aeiou": #función que convierte a minuscula
 
             contador_vocales +=1
 
