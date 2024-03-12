548f664f960b2acb666a7e386861a205bdfffe21
Mikael Tiba <mikaeltiba04@gmail.com>
2024-03-12 18:52:39 -0400

    Updating files

--- modulo02/AC-funciones-arreglos/funciones/ej10.py
+++ modulo02/AC-funciones-arreglos/funciones/ej10.py
@@ -2,7 +2,7 @@ def contar_apariciones_letra(cadena, letra):
     contador_letra = 0 #almacena la aparición de la letra escogida
 
     for char in cadena:
-        if char.lower() in letra.lower(): 
+        if char.lower() in letra.lower(): #convierte la cadena en minúscula y letra.lower() busca la a
             contador_letra +=1 #se incrementa
             
     return contador_letra
