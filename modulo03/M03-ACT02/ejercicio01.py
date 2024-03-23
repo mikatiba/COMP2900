414cc59b23c97b93ecf1e34ddf9d296b57596014
Mikael Tiba <mikaeltiba04@gmail.com>
2024-03-23 13:50:20 -0400

    Update

---
+++ modulo03/M03-ACT02/ejercicio01.py
@@ -0,0 +1,30 @@
+import timeit
+
+#enfoque 1
+def search_for(number, numbers):
+    for i in range(len(numbers)):
+        if numbers [i] == number:
+            return i
+    return -1
+
+ #enfoque 2
+def search_index(number, numbers):
+     try:
+         return numbers.index(number) 
+     except ValueError:
+         return -1
+
+
+numbers = list(range(1, 1001))
+
+time_for = timeit.timeit(lambda: search_for(1000,numbers), number=10000)
+
+print(f"Enfoque 1: {time_for}")
+
+
+numbers = list(range(1, 1001))
+
+time_index = timeit.timeit(lambda: search_index(1000,numbers), number=10000)
+
+print(f"Enfoque 2: {time_index}")
+
